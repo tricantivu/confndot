caab8c9428dd1a8bdbbdc0483eeb25c03590a1eb
Alejandro Cervera <96702705+tricantivu@users.noreply.github.com>
2023-10-21 17:19:53 -0500

    Remove statement setting option for accounts.google.com

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -77,6 +77,5 @@ config.set("content.javascript.enabled", True, "devtools://*")
 config.set("content.javascript.enabled", True, "chrome://*/*")
 config.set("content.javascript.enabled", True, "chrome-devtools://*")
 
-config.set("content.headers.user_agent", "Mozilla/5.0 ({os_info}; rv:90.0) Gecko/20100101 Firefox/90.0", "https://accounts.google.com/*")
 config.set("content.headers.user_agent", "Mozilla/5.0 ({os_info}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99 Safari/537.36", "https://*.slack.com/*")
 config.set("content.headers.user_agent", "Mozilla/5.0 ({os_info}) AppleWebKit/{webkit_version} (KHTML, like Gecko) {upstream_browser_key}/{upstream_browser_version} Safari/{webkit_version}", "https://web.whatsapp.com/")
